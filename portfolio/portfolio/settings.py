06a50bd41e83394546f6b9a9514db6a01b4456c9
mercyogalo <143324456+mercyogalo@users.noreply.github.com>
2024-11-05 13:39:08 +0300

    created all the required pages for my website

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'portfoliodesign',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
