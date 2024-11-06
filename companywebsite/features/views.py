863d2b001faf37f41983a6f09b9c194c7d8eab1a
mercyogalo <143324456+mercyogalo@users.noreply.github.com>
2024-11-06 20:31:38 +0300

    written all the functions for the different pages

--- companywebsite/features/views.py
+++ companywebsite/features/views.py
@@ -6,3 +6,33 @@ def index(request):
 
 def about(request):
     return render(request,'about.html')
+
+def blogDetails(request):
+    return render(request, 'blog-details.html')
+
+def blog(request):
+    return render(request,'blog.html')
+
+def contact(request):
+    return render(request,'contact.html')
+
+def portfolioDetails(request):
+    return render(request, 'portfolio-details.html')
+
+def portfolio(request):
+    return render(request, 'portfolio.html')
+
+def pricing(request):
+    return render(request, 'pricing.html')
+
+def serviceDetails(request):
+    return render(request, 'service-details.html')
+
+def  starterPage(request):
+    return render(request, 'starter-page.html')
+
+def team(request):
+    return render(request, 'team.html')
+
+def testimonials(request):
+    return render(request,'testimonials.html')
